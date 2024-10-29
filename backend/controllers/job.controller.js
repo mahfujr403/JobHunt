214791b1bfa47a0e77d772adec9c114c522959d3
mahfuj <mahfujr403@gmail.com>
2024-10-29 13:24:55 +0600

    first commit

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,171 @@
+import { Job } from "../models/job.model.js";
+
+// admin post krega job
+export const postJob = async (req, res) => {
+	try {
+		const {
+			title,
+			description,
+			requirements,
+			salary,
+			location,
+			jobType,
+			experience,
+			position,
+			companyId,
+		} = req.body;
+		const userId = req.id;
+
+		if (
+			!title ||
+			!description ||
+			!requirements ||
+			!salary ||
+			!location ||
+			!jobType ||
+			!experience ||
+			!position ||
+			!companyId
+		) {
+			return res.status(400).json({
+				message: "Somethin is missing.",
+				success: false,
+			});
+		}
+		const job = await Job.create({
+			title,
+			description,
+			requirements: requirements.split(","),
+			salary: Number(salary),
+			location,
+			jobType,
+			experienceLevel: experience,
+			position,
+			company: companyId,
+			created_by: userId,
+		});
+		return res.status(201).json({
+			message: "New job created successfully.",
+			job,
+			success: true,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+// student k liye
+export const getAllJobs = async (req, res) => {
+	try {
+		const keyword = req.query.keyword || "";
+		const query = {
+			$or: [
+				{ title: { $regex: keyword, $options: "i" } },
+				{ description: { $regex: keyword, $options: "i" } },
+			],
+		};
+		const jobs = await Job.find(query)
+			.populate({
+				path: "company",
+			})
+			.sort({ createdAt: -1 });
+		if (!jobs) {
+			return res.status(404).json({
+				message: "Jobs not found.",
+				success: false,
+			});
+		}
+		return res.status(200).json({
+			jobs,
+			success: true,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+// student
+export const getJobById = async (req, res) => {
+	try {
+		const jobId = req.params.id;
+		const job = await Job.findById(jobId).populate({
+			path: "applications",
+		});
+		if (!job) {
+			return res.status(404).json({
+				message: "Jobs not found.",
+				success: false,
+			});
+		}
+		return res.status(200).json({ job, success: true });
+	} catch (error) {
+		console.log(error);
+	}
+};
+// admin kitne job create kra hai abhi tk
+export const getAdminJobs = async (req, res) => {
+	try {
+		const jobId = req.params.id;
+		const job = await Job.findById(jobId).populate({
+			path: "applications",
+		});
+		if (!job) {
+			return res.status(404).json({
+				message: "Jobs not found.",
+				success: false,
+			});
+		}
+		return res.status(200).json({ job, success: true });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateJob = async (req, res) => {
+	try {
+		const jobId = req.params.id;
+		const {
+			title,
+			description,
+			requirements,
+			salary,
+			location,
+			jobType,
+			experience,
+			position,
+			companyId,
+		} = req.body;
+		const userId = req.id;
+		const job = await Job.findById(jobId);
+		if (!job) {
+			return res.status(404).json({
+				message: "Job not found.",
+				success: false,
+			});
+		}
+		if (job.created_by.toString() !== userId) {
+			return res.status(401).json({
+				message: "You are not authorized to update this job.",
+				success: false,
+			});
+		}
+		const updatedJob = await Job.findByIdAndUpdate(jobId, {
+			title,
+			description,
+			requirements: requirements.split(","),
+			salary,
+			location,
+			jobType,
+			experienceLevel: experience,
+			position,
+			company: companyId,
+			created_by: userId,
+			new: true,
+		});
+		return res.status(200).json({
+			message: "Job updated successfully.",
+			updatedJob,
+			success: true,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
