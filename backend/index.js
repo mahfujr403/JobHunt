93f9d7679058de76a1587ca78ea9bc227da7ebfb
mahfuj <mahfujr403@gmail.com>
2024-11-21 13:47:20 +0600

    Url Update

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+	origin: "https://jobhunt-dstu.onrender.com/",
+	credentials: true,
+};
 
 app.use(cors(corsOptions));
 
